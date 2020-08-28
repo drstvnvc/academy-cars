cfa7db1e7ca5fe43e5e472fa930793c535a274de
Drago Stevanovic <drago.s@vivifyideas.com>
2020-08-28 21:11:36 +0200

    Change blade templates so they extend the main app layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,6 @@
-<html>
-  <head>
-    <title>About</title>
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="badge-success">About us</h1>
-    </div>
-    <script src="{{ URL::asset('js/app.js') }}"/>
-  </body>
-</html>
+@extends('layouts.app')
+@section('title', 'About')
+
+@section('content')
+  <h1>About us</h1>
+@endsection
--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -1,13 +1,11 @@
-<html>
-  <head>
-    <title>About</title>
-  </head>
-  <body>
-    <h1>Cars</h1>
-    @foreach ($cars as $car)
-      <div>
-        {{$car->producer}} - {{$car->title}} ({{$car->number_of_doors}} doors)
-      </div>
-    @endforeach
-  </body>
-</html>
+@extends('layouts.app')
+@section('title', 'About')
+
+@section('content')
+  <h1>Cars</h1>
+  @foreach ($cars as $car)
+    <div>
+      {{$car->producer}} - {{$car->title}} ({{$car->number_of_doors}} doors)
+    </div>
+  @endforeach
+@endsection
