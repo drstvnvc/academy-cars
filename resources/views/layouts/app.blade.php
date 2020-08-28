f6dfcf62a2ee0769ef6ff702e384de92768b941d
Drago Stevanovic <drago.s@vivifyideas.com>
2020-08-28 21:09:50 +0200

    Add bootstrap to the project. Create a layout for all blade templates. Include bootstrap in the layout template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
 <html>
   <head>
-    <title>About</title>
+    <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
   </head>
   <body>
     <div class="container">
-      <h1 class="badge-success">About us</h1>
+      @yield('content')
     </div>
     <script src="{{ URL::asset('js/app.js') }}"/>
   </body>
