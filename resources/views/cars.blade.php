f7f246f5cb71c869e404c08b047d732c26e3952b
Drago Stevanovic <drago.s@vivifyideas.com>
2020-08-28 21:15:52 +0200

    Use bootstrap to display cars in a table layout

--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -3,9 +3,29 @@
 
 @section('content')
   <h1>Cars</h1>
+  <div class="row">
+    <div class="col-md-4">
+      <h3>Producer</h3>
+    </div>
+    <div class="col-md-4">
+      <h3>Name</h3>
+    </div>
+    <div class="col-md-4">
+      <h3>Number of doors</h3>
+    </div>
+  </div>
+
   @foreach ($cars as $car)
-    <div>
-      {{$car->producer}} - {{$car->title}} ({{$car->number_of_doors}} doors)
+    <div class="row">
+      <div class="col-md-4">
+        {{$car->producer}}
+      </div>
+      <div class="col-md-4">
+        {{$car->title}}
+      </div>
+      <div class="col-md-4">
+        {{$car->number_of_doors}} doors
+      </div>
     </div>
   @endforeach
 @endsection
